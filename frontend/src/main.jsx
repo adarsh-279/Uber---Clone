acb7b997f5e30e5bfced8134c10000e8914fa3ed
adarsh-279 <dev.adarsh279@gmail.com>
2025-11-29 19:14:48 +0530

    user protected route & user context fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import AppContext from './context/AppContext.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import UserContext from "./context/UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-    <AppContext>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </AppContext>
+  <UserContext>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </UserContext>
 );
