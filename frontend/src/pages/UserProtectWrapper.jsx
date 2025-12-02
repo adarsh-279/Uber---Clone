a598a74859ee307dcea2327b70dd8269ec911c7a
adarsh-279 <dev.adarsh279@gmail.com>
2025-12-02 22:41:14 +0530

    Updated authentication login token issue

--- frontend/src/pages/UserProtectWrapper.jsx
+++ frontend/src/pages/UserProtectWrapper.jsx
@@ -2,17 +2,37 @@ import React, { useContext } from 'react'
 import { UserDataContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
+import { useState } from 'react'
 
 const UserProtectWrapper = ({ children }) => {
 
     const token = localStorage.getItem('token')
     const navigate = useNavigate()
+    const { user, setUser } = useContext(UserDataContext)
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         if (!token) {
-                navigate('/captain/login')
+            navigate('/user/login')
+        }
+
+        axios.get(`${import.meta.env.VITE_BASE_URL}/user/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
             }
-    }, [token])
+        }).then((response) => {
+            setUser(response.data.user)
+            setIsLoading(false)
+        }).catch((err) => {
+            console.log(err)
+            localStorage.removeItem('token')
+            navigate("/user/login");
+        })
+        }, [token])
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
 
     return (
         <>
