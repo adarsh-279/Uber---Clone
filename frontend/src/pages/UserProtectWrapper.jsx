512079af3dda0693d92046e05f8541c65d13cc88
adarsh-279 <dev.adarsh279@gmail.com>
2025-12-02 21:59:02 +0530

    fixed user login issue

--- frontend/src/pages/UserProtectWrapper.jsx
+++ frontend/src/pages/UserProtectWrapper.jsx
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react'
 import { UserDataContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const UserProtectWrapper = ({ children }) => {
 
     const token = localStorage.getItem('token')
     const navigate = useNavigate()
 
-    if (!token) {
-        navigate('/user/login')
-    }
+    useEffect(() => {
+        if (!token) {
+                navigate('/captain/login')
+            }
+    }, [token])
+
     return (
         <>
             {children}
